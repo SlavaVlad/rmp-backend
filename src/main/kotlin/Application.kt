859119b25f9f48e8b9be7b6789400f7594c385ac
SlavaVlad <nauka.2.0.vs@gmail.com>
2026-01-15 06:03:39 +0300

    Порт 8080

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -5,7 +5,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, port = 5000, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
